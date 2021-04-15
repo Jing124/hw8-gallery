e4420e6e633ace116479acbebbb1ffdbd7f60c20
Jing Wang <wjing.archdesi@gmail.com>
2021-04-15 18:25:45 -0400

    added files

---
+++ script.js
@@ -0,0 +1,27 @@
+let currentSlideIndex = -1;
+const slides = document.getElementsByClassName('mySlides');
+
+setInterval(() => {
+    nextSlide();
+}, 3000);
+
+function prevSlide() {
+    currentSlideIndex--;
+    currentSlideIndex =
+        currentSlideIndex < 0 ? slides.length - 1 : currentSlideIndex;
+    showSlide();
+}
+
+function nextSlide() {
+    currentSlideIndex++;
+    currentSlideIndex =
+        currentSlideIndex >= slides.length ? 0 : currentSlideIndex;
+    showSlide();
+}
+
+function showSlide() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[currentSlideIndex].style.display = 'block';
+}
